3e9ee95c509714ba92857a54448ba97deae07e4b
Ben Stephens <BKStephens@outlook.com>
2015-04-13 19:52:09 -0700

    Updated StringsAndBinaries-4.exs to be more dynamic

--- BKStephens/ch11/StringsAndBinaries-4.exs
+++ BKStephens/ch11/StringsAndBinaries-4.exs
@@ -1,16 +1,9 @@
 defmodule MyString do
-  @operations %{
-    ?+ => &(&1 + &2),
-    ?- => &(&1 - &2),
-    ?/ => &(&1 / &2),
-    ?* => &(&1 * &2)
-  }
-
   def calculate(expression) do
     #Split the string the first time +-* or / is found  
     { left, rest } = Enum.split_while(expression, &(!(&1 in '+-*/')))
     [ operator | right ] = rest  
-    @operations[operator].(to_int(left), to_int(right))    
+    apply :erlang, List.to_atom([operator]), [to_int(left), to_int(right)]
   end
   
   defp to_int(sqs), do: ~w(#{sqs}) |> List.first |> String.to_integer
