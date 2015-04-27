2b5db0fb42a318fdcacf0c163155176702b269bc
Kyle Rippey <kyle@avvo.com>
2015-04-26 23:31:18 -0700

    Added docs and doc tests.

--- kylerippey/ch13/issues/lib/issues/github_issues.ex
+++ kylerippey/ch13/issues/lib/issues/github_issues.ex
@@ -15,7 +15,16 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}), do: {:ok, :jsx.decode(body)}
-  def handle_response({:ok, %{status_code: _, body: body}}), do: {:error, :jsx.decode(body)}
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
+   {:ok, :jsx.decode(body)}
+ end
+
+  def handle_response({:ok, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
+    {:error, :jsx.decode(body)}
+  end
+
   def handle_response({:error, err}), do: {:error, err}
 end
--- kylerippey/ch13/issues/lib/issues/table_formatter.ex
+++ kylerippey/ch13/issues/lib/issues/table_formatter.ex
@@ -1,6 +1,14 @@
 defmodule Issues.TableFormatter do
   import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
 
+  @doc """
+  Takes a list of row data, where each row is a HashDict, and a
+  list of headers. Prints a table to STDOUT of the data from each
+  row identified by each header. That is, each header identifies
+  a column, and those columns are extracted and printed from the
+  rows. We calculate the width of each column to fit the longest
+  element in that column.
+  """
   def print_table_for_columns(rows, headers) do
     data_by_columns = split_into_columns(rows, headers)
     column_widths = widths_of(data_by_columns)
@@ -16,21 +24,62 @@ defmodule Issues.TableFormatter do
     end
   end
 
+  @doc """
+  Return a binary (string) version of our parameter.
+
+  ## Examples
+    iex> Issues.TableFormatter.printable("a")
+    "a"
+    iex> Issues.TableFormatter.printable(99)
+    "99"
+  """
   def printable(str) when is_binary(str), do: str
   def printable(str), do: to_string(str)
 
+  @doc """
+  Given a list containing sublists, where each sublist contains the
+  data for a column, return a list containing the maximum width of
+  each column.
+
+  ## Example
+    iex> data = [ [ "cat", "wombat", "elk"], ["mongoose", "ant", "gnu"]]
+    iex> Issues.TableFormatter.widths_of(data)
+    [ 6, 8 ]
+  """
   def widths_of(columns) do
     for column <- columns, do: column |> map(&String.length/1) |> max
   end
 
+  @doc """
+  Return a format string that hard codes the widths of a set of
+  columns. We put `" | "` between each column.
+
+  ## Example
+    iex> widths = [5,6,99]
+    iex> Issues.TableFormatter.format_for(widths)
+    "~-5s | ~-6s | ~-99s~n"
+  """
   def format_for(column_widths) do
     map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
   end
 
+  @doc """
+  Generate the line that goes below the column headings. It is a string of hyphens, with + signs where the vertical bar between the columns goes.
+
+  ## Example
+    iex> widths = [5,6,9]
+    iex> Issues.TableFormatter.separator(widths)
+    "------+--------+----------"
+  """
   def separator(column_widths) do
     map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
   end
 
+  @doc """
+  Given a list containing rows of data, a list containing the
+  header selectors, and a format string, write the extracted
+  data under control of the format string.
+  """
   def puts_in_columns(data_by_columns, format) do
     data_by_columns
     |> List.zip
