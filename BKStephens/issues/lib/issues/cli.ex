605a5d01ed57e78cc7155887076b80478d4b3aad
Ben Stephens <BKStephens@outlook.com>
2015-04-26 20:16:20 -0700

    Issues Table Formatter

--- BKStephens/issues/lib/issues/cli.ex
+++ BKStephens/issues/lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -46,6 +48,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
