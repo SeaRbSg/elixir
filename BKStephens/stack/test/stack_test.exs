f42d9d92413d6159b29bc28107862480940fef24
Ben Stephens <BKStephens@outlook.com>
2015-05-18 19:04:23 -0700

    OTP-Applications-2

--- BKStephens/stack/test/stack_test.exs
+++ BKStephens/stack/test/stack_test.exs
@@ -1,7 +1,27 @@
 defmodule StackTest do
   use ExUnit.Case
+ 
+  setup do
+    #this does not work yet... it is expecting an { :ok, dict }
+    #:sys.replace_state(Stack.Server, fn _ -> { :ok, ["Howdy", "ho", "folks"] } end)
+  end
+  
+  test "the stack has initial values" do
+    { stack, pid } = :sys.get_state Stack.Server
+    assert stack == ["Howdy", "ho", "folks"]
+    refute pid == nil
+  end
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "can push onto the stack" do
+    Stack.Server.push "!!!"
+    { stack, pid } = :sys.get_state Stack.Server
+    assert stack == ["!!!", "Howdy", "ho", "folks"]
+  end
+
+  test "can pop items off stack" do
+    assert Stack.Server.pop == "!!!"
+    assert Stack.Server.pop == "Howdy"
+    assert Stack.Server.pop == "ho"
+    assert Stack.Server.pop == "folks"
   end
 end
